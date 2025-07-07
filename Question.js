04567611c6d60a222d1e35a1c8b12304308b1ada
srijesh bista <srijeshbista367@gmail.com>
2025-07-07 15:46:54 +0545

    first commit

---
+++ Question.js
@@ -0,0 +1,89 @@
+// // Question 1
+// for (let i = 1; i<= 5; i++) {
+//     console.log(i);
+
+// }
+// //Question 2
+// let number=2;
+// if (number % 2 === 0){
+//     console.log("The Number Is Even");
+// }
+// else{
+//     console.log("The Number Is Odd")
+// }
+
+// //Question 3
+// let i = 10;
+// while (i >= 1){
+//     console.log(i);
+//     i--
+// }
+
+
+// //Question 4
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+//     if (i % 2 === 0) {
+//         console.log("Even Number")
+//     }
+    
+// }
+// //Question 5
+// let i =0;
+// while (i >= 0) {
+   
+//     if(i>0){
+//         console.log("Positive Number")
+//     }
+//      else if(i<=0) {
+//         console.log("Stop")
+//     }else {
+//         console.log("Negative")
+//     }
+   
+//     i--
+   
+    
+// }
+ 
+
+// Question for Function 
+
+// //Question 1
+
+// function cname (num){
+//     if(num%2==0){
+//         console.log("Even Number");
+//     }else{
+//         console.log("Odd Number");
+//     }
+// }
+//     cname(8);
+
+
+// //Question 2
+
+//     for(let i=1;i<=10;i++){
+//         console.log(i)
+//     }
+
+// //Question3
+// let num=12345;
+// sum=0;
+// while (num>0){
+//     let digite = num%10;
+//     sum +=digite;
+//     num=Math.floor(num/10);
+// }
+// console.log(sum);
+
+// //Question 4
+
+// function number (nber){
+//  if(nber<=10){
+//         console.log("Smaller Number")
+//  }   else{
+//     console.log("Larger Number")
+//  }
+// }
+// number(20);
